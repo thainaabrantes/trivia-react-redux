35ceafffd0fe1dc6210b3a92a205860e07c2e359
Joey Hetfield <joeydefenix@hotmail.com>
2023-01-15 14:58:51 -0300

    arrumando lint

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -114,20 +114,17 @@ class Games extends Component {
     const { countQuestion } = this.state;
     const { history } = this.props;
     const lastQuestion = 5;
-    const secondsfinal = 500;
     this.setState((prevState) => ({
       countQuestion: prevState.countQuestion + 1,
     }));
     if (countQuestion === lastQuestion) {
       history.push('/feedback');
     }
-    setTimeout(() => {
-      this.setState({
-        clicked: false,
-      });
-      this.fetchAnswers();
-      this.resetSeconds();
-    }, secondsfinal);
+    this.setState({
+      clicked: false,
+    });
+    this.fetchAnswers();
+    this.resetSeconds();
   };
 
   //  Fiz um aninhamento de expressão ternária para fazer a classe, porém o Lint me obrigou a fazer isso
